3548857c0890f8e2509b6543fe2798956e9a1bdf
tasifaiman21 <tasifaiman21@gmail.com>
2022-05-22 19:12:33 +0600

    inital

---
+++ calculation.js
@@ -0,0 +1,25 @@
+let display = document.getElementById("display");
+
+let buttons = Array.from(document.getElementsByClassName("button"));
+
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "C":
+        display.innerText = " ";
+        break;
+
+      case "←":
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1);
+        }
+        break;
+      case "=":
+        display.innerText = eval(display.innerText);
+        break;
+
+      default:
+        display.innerText = display.innerText + e.target.innerText;
+    }
+  });
+});
